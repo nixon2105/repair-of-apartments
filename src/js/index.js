36205c947c4d8a2dea336db2904de27c9c16f66b
nixon2105 <radivilov1991@mail.ru>
2021-03-06 19:05:01 +0300

    feat: add the faq section

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
 // import '../scss/style.css';
 import '../scss/style.scss';
+
+let coll = document.getElementsByClassName('collapsible');
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  });
+}
